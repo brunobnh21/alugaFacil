6bd2686f8be17977b9806b9e509e9b280049f652
brunobnh21 <75455867+brunobnh21@users.noreply.github.com>
2020-12-03 20:28:42 -0300

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -29,3 +29,10 @@ module.exports = function (config) {
     singleRun: false
   });
 };
+prompt> git add -i
+     staged unstaged path
+     1: unchanged +1/-1 index.html
+      *** Commands ***
+     1: status 2: update 3: revert 4: add untracked
+     5: patch 6: diff 7: quit 8: help
+What now>
